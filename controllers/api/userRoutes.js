c11b6cd6879a485b134b439e8e522d50d19bf6d4
Emil1577 <emilronquillo@gmail.com>
2023-02-10 14:17:39 -0800

    updating routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,7 +12,7 @@ router.post('/signup', async (req, res) => {
         });
 //This is to save the user as loggedIn
         req.session.save(() => {
-            loggedIn = req.session.loggedIn;
+            req.session.loggedIn=true,
             res.status(200).json(userData);
         });
 console.log(req.session); //to check if the user successfully loggedIn with the information provided
@@ -23,41 +23,41 @@ console.log(req.session); //to check if the user successfully loggedIn with the 
 });
 
 
-// //LOGIN
-// router.post('/login', async (req, res) => {
-//     try {
-//         const userData = await User.findOne({
-//             where: {
-//                 email: req.body.email
-//             }
-//         });
+//LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
 
-//         if (!userData) {
-//             res
-//                 .status(404)
-//                 .json({ message: 'Incorrect email or password. Please try again!' });
-//             return;
-//         }
-//         const validPassword = await userData.checkPassword(req.body.password);
+        if (!userData) {
+            res
+                .status(404)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
 
-//         if (!validPassword) {
-//             res
-//                 .status(404)
-//                 .json({ message: 'Incorrect email or password. Please try again!' });
-//             return;
-//         }
-// //This is to save the user as loggedIn
-//         req.session.save(() => {
-//             loggedIn = req.session.loggedIn;
-//             res.status(200)
-//             .json({ user: userData, message: 'You are now logged in!' });
-//         });
+        if (!validPassword) {
+            res
+                .status(404)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+//This is to save the user as loggedIn
+        req.session.save(() => {
+            req.session.loggedIn=true,
+            res.status(200)
+            .json({ user: userData, message: 'You are now logged in!' });
+        });
 
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 
 // //Logout
