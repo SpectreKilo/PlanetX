7df850353ec2598c210de26ef1bdb54af41e6046
SpectreKilo <johnho8788@gmail.com>
2023-02-10 13:19:22 -0500

    half created user and index models added genre and subgenre model files

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const User = require("./User");
+const BlogPost = require("./BlogPost");
+const Comment = require("./Comment");
+
+Comment.belongsTo(BlogPost, {
+    foreignKey: "topic_id",
+    onDelete: "CASCADE"
+})
+
+BlogPost.hasMany(Comment, {
+    foreignKey: "topic_id"
+})
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+BlogPost.belongsTo(User, {
+    foreignKey: "user_id"
+})
