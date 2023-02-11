ec74716649888b938f9ab78fcdb6980587eede4a
SpectreKilo <johnho8788@gmail.com>
2023-02-11 14:08:46 -0500

    removed galaxy route from index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const galaxyRoutes = require('./galaxyRoutes');
 const planetRoutes = require('./planetRoutes')
 
-
-router.use('/galaxy', galaxyRoutes);
-router.use('/planets', planetRoutes)
-router.use('/users', userRoutes)
+router.use('/planets', planetRoutes);
+router.use('/users', userRoutes);
 
 
 module.exports = router;
