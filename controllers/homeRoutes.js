620d083b76772f8287668595dd67967cfbf18288
SpectreKilo <johnho8788@gmail.com>
2023-02-11 13:26:42 -0500

    added route to get subgenres by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment, BlogPost, User } = require('../models');
+const { Comment, BlogPost, User, SubGenre } = require('../models');
 const withAuth = require('../utils/auth')
 
 
@@ -7,7 +7,33 @@ router.get('/', async (req, res) => {
     res.render('universe');
 });
 
-//! add get post by id
+// add get post by id
+
+
+// Gets specific subgenres by id
+router.get('/planet/:id', withAuth, async (req, res) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+        return;
+    }
+    try {
+        const subGenre = await SubGenre.findByPk(req.params.id, {
+            include: [
+                BlogPost,
+            ],
+        });
+    const planets = subGenre.get({ plain:true });
+        console.log(planets)
+    res.render('galaxies', {
+        planets,
+        loggedIn: req.session.loggedIn
+    });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// gets post by specific ID
 router.get('/moon/:id', withAuth, async (req, res) => {
     if (!req.session.logged_in) {
         res.redirect('/login');
