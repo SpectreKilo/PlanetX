aed49cc444247c7d72f12aeda22680825f75a4fe
SpectreKilo <johnho8788@gmail.com>
2023-02-11 13:11:35 -0500

    added get post by unique id route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Comment, BlogPost, User } = require('../models');
 const withAuth = require('../utils/auth')
 
 
@@ -7,7 +8,34 @@ router.get('/', async (req, res) => {
 });
 
 //! add get post by id
+router.get('/moon/:id', withAuth, async (req, res) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+        return;
+    }
+    try {
+        const blogData = await BlogPost.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                model: Comment,
+                    include: [
+                        User
+                    ]
+                },
 
+            ],
+        });
+    const blogPosts = blogData.get({ plain:true });
+        console.log(blogPosts)
+    res.render('moon_post', {
+        blogPosts,
+        loggedIn: req.session.loggedIn
+    });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.get('/login', (req, res) => {
 if(req.session.loggedIn) {
