f6418885f06cb82f26d10024e9dfc7521d4d4cfe
BrettSantor <b.santor27@gmail.com>
2023-02-11 10:29:26 -0800

    create post route complete

--- controllers/api/planetRoutes.js
+++ controllers/api/planetRoutes.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const { BlogPost, User } = require('../../models')
+const withAuth = require('../../utils/auth')
 
 //todo Post routes
 //! add create post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await BlogPost.create({
+            ...req.body,
+            // topic: req.body.topic,
+            // content: req.body.content,
+            // date_created: req.body.date_created,
+            user_id: req.session.user_id,
+        });
+
+        const post = newPost.get({plain: true});
+        res.render('moonForm',{ post, 
+        loggedIn: req.session.loggedIn});
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 //! add edit post
 //! add delete post
 
