a26af709468d6f03ce7668d5a8dbc27461dd0b06
BrettSantor <b.santor27@gmail.com>
2023-02-11 16:13:48 -0800

    link seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,11 @@
 const sequelize = require('../config/connection');
-const { User, BlogPost } = require('../models');
+const { User, BlogPost, Comment, Genre, SubGenre } = require('../models');
 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
+const galaxyData = require('./galaxyData.json');
+const planetData = require('./planetData.json');
+const flagData = require('./flagData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -11,6 +14,9 @@ const seedDatabase = async () => {
         individualHooks: true,
         returning: true,
     });
+    
+    const galaxy = await Genre.bulkCreate(galaxyData)
+    const planet = await SubGenre.bulkCreate(planetData)
 
     for (const blogPost of blogData) {
         await BlogPost.create({
@@ -18,6 +24,8 @@ const seedDatabase = async () => {
             user_id: users[Math.floor(Math.random() * users.length)].id,
         })
     }
+
+    const flag = await Comment.bulkCreate(flagData)
 
     process.exit(0);
 };
