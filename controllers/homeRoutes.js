6039cde3670fa5b7ec0a6117194b8dd4883a6189
Emil1577 <emilronquillo@gmail.com>
2023-02-11 20:32:21 -0800

    cleaning up some console log

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment, BlogPost, User, SubGenre, Genre} = require('../models');
+const { Comment, BlogPost, User, SubGenre, Genre } = require('../models');
 const withAuth = require('../utils/auth')
 
 
@@ -8,20 +8,20 @@ router.get('/', async (req, res) => {
         const planetData = await Genre.findAll({
             include: [
                 {
-                    model:SubGenre,
+                    model: SubGenre,
                     attributes: ["subgenre_name"]
                 },
             ],
         });
-        const subgenres = planetData.map((subGenre) => subGenre.get({ plain:true }));
-    console.log(subgenres)
-    res.render('homepage', {
-        subgenres,
-        loggedIn: req.session.loggedIn
-    });
-} catch (err) {
-    res.status(500).json(err);
-}
+        const subgenres = planetData.map((subGenre) => subGenre.get({ plain: true }));
+        console.log(subgenres)
+        res.render('homepage', {
+            subgenres,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/mothership', withAuth, async (req, res) => {
@@ -37,45 +37,41 @@ router.get('/mothership', withAuth, async (req, res) => {
             //],
         });
         console.log(shipData)
-        const mothership = shipData.get({ plain:true });
-    console.log(mothership)
-    res.render('mothership', {
-        mothership,
-        loggedIn: req.session.loggedIn
-    });
-} catch (err) {
-    res.status(500).json(err);
-}
+        const mothership = shipData.get({ plain: true });
+        console.log(mothership)
+        res.render('mothership', {
+            mothership,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Gets specific subgenres by id
 router.get('/planet/:id', withAuth, async (req, res) => {
 
-console.log('hit planet')
-
-console.log(req.params.id)
+    console.log(req.params.id)
 
     try {
+        const subGenreData = await SubGenre.findByPk(req.params.id, {
 
-       const subGenreData = await SubGenre.findByPk(req.params.id, {
-     //   attributes: {subgenre_name},
-        // include: [{ model: Project }],
+            // we need to specify attribute?
+
         });
-    const planet = subGenreData.get({ plain:true });
+        const planet = subGenreData.get({ plain: true });
 
-        console.log('planetspage')
-        console.log(subGenreData)
-       console.log(planet)
-    res.render('homepage' , {
-        layout:'main',
-       planet,
-        loggedIn: req.session.loggedIn
-    });
+        console.log(planet)
+
+        res.render('homepage', {
+            layout: 'main',
+            planet,
+            loggedIn: req.session.loggedIn
+        });
 
     } catch (err) {
         res.status(500).json(err);
     }
-
 });
 
 // gets post by specific ID
@@ -89,7 +85,7 @@ router.get('/moon/:id', withAuth, async (req, res) => {
             include: [
                 User,
                 {
-                model: Comment,
+                    model: Comment,
                     include: [
                         User
                     ]
@@ -97,22 +93,22 @@ router.get('/moon/:id', withAuth, async (req, res) => {
 
             ],
         });
-    const blogPosts = blogData.get({ plain:true });
+        const blogPosts = blogData.get({ plain: true });
         console.log(blogPosts)
-    res.render('moon_post', {
-        blogPosts,
-        loggedIn: req.session.loggedIn
-    });
+        res.render('moon_post', {
+            blogPosts,
+            loggedIn: req.session.loggedIn
+        });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
 router.get('/login', (req, res) => {
-if(req.session.loggedIn) {
-    res.redirect('/')
-    return;
-}
+    if (req.session.loggedIn) {
+        res.redirect('/')
+        return;
+    }
     res.render('login');
 })
 
